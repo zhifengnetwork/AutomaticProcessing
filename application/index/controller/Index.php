cdc5c7562aaead5f6442cc98fa3ed18f23d4e7e1
FZJ <yudwcom@163.com>
2019-05-30 21:15:09 +0800

    测试

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,19 +8,30 @@ class Index
 {
     public function index()
     {
-
-   
         Rollbar::init(
             array(
                 'access_token' => 'eccb4e2e66634e26becab4fb1f488132',
                 'environment' => 'production'
             )
         );
+        Rollbar::log(Level::info(), '出错了，大错误');
+       
 
-        
-        
-        Rollbar::log(Level::info(), 'Test info message');
-        throw new Exception('Test exception');
+
+        return '批量自动化处理';
+    }
+
+
+    public function aaa()
+    {
+        Rollbar::init(
+            array(
+                'access_token' => 'eccb4e2e66634e26becab4fb1f488132',
+                'environment' => 'production'
+            )
+        );
+        Rollbar::log(Level::info(), 'aaa模块 出错了，大错误');
+       
 
 
         return '批量自动化处理';
